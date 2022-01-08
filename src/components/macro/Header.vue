69a250c55b969056fa45e7b5c082db7cf06c9434
Alessandro <luisialessandro95@gmail.com>
2022-01-08 17:20:50 +0100

    static searchbar

--- src/components/macro/Header.vue
+++ src/components/macro/Header.vue
@@ -1,19 +1,46 @@
 <template>
     <header>
-        <h2>Header</h2>
-        <i class="fas fa-dog"></i>
-        <i class="fas fa-cat"></i>
+        <nav>
+            <i class="fab fa-spotify"></i>
+            <input type="text" placeholder="Search" v-model="search" @keyup.enter="searchCharacter()">
+        </nav>
     </header>
 </template>
 
 <script>
 export default {
-    name: 'Header'
+    name: 'Header',
+    data(){
+        return{
+            search: ''
+        }
+    },
+    methods: {
+        searchCharacter(){
+            alert('hello!');
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
-    h2{
-        color: blue;
+    nav{
+        height: 50px;
+        background-color: #2e3a46;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 20px;
+
+        .fa-spotify{
+            font-size: 37px;
+            color: #1ed760;
+        }
+
+        input{
+            padding: 5px;
+            border: none;
+            border-radius: 20px;
+        }
     }
 </style>
